5b05b6a03d9989e4d2fbea8c280b41466da54fe4
Eduardo Francisco <duudu.0304@gmail.com>
2026-06-04 13:29:30 -0300

    Payment não tava rodando sem o kafka config para produzir as mensagens. Testes ainda não realizados, próximo passo - dockerização

--- Payment-Service/src/main/java/br/com/infnet/payment/service/PaymentService.java
+++ Payment-Service/src/main/java/br/com/infnet/payment/service/PaymentService.java
@@ -5,30 +5,34 @@ import br.com.infnet.payment.domain.model.Payment;
 import br.com.infnet.payment.dto.PaymentRequest;
 import br.com.infnet.payment.dto.PaymentResponse;
 import br.com.infnet.payment.repository.PaymentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 @Service
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public PaymentService(PaymentRepository paymentRepository, KafkaTemplate<String, Object> kafkaTemplate) {
+    public PaymentService(PaymentRepository paymentRepository,
+                          KafkaTemplate<String, String> kafkaTemplate,
+                          ObjectMapper objectMapper) {
         this.paymentRepository = paymentRepository;
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public PaymentResponse create(PaymentRequest request) {
 
         try {
-            // número aleatório de tempo
             long tempoAleatorio = ThreadLocalRandom.current().nextLong(3000, 8000);
             Thread.sleep(tempoAleatorio);
 
@@ -47,7 +51,14 @@ public class PaymentService {
         Payment savedPayment = paymentRepository.save(payment);
         PaymentResponse response = toResponse(savedPayment);
 
-        kafkaTemplate.send("payment-processed", savedPayment.getOrderId().toString(), response);
+        String jsonMessage = null;
+        try {
+            jsonMessage = objectMapper.writeValueAsString(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        kafkaTemplate.send("payment-processed", savedPayment.getOrderId().toString(), jsonMessage);
+        System.out.println("Mensagem enviada com sucesso pelo KafkaConfig: " + jsonMessage);
 
         return response;
     }
